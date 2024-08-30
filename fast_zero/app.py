e45157162ace60a9923dac804f9f5f53b694f50f
Paulo Arruda <pauloarruda5@protonmail.com>
2024-08-30 00:06:20 -0300

    Aula 3 - CRUD (43:00)

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fast_zero.schemas import Message, UserSchema, UserPublic
+from http import HTTPStatus
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundoh'}
+
+@app.post('/users/',status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
---
+++ fast_zero/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr
+
+class Message(BaseModel):
+    message: str
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserDB(UserSchema):
+    id: int
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
